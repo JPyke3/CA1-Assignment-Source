7d10363f364d6d35078c1c4968d15f21e42f6cb3
Jacob Pyke <pyke.jacob1@gmail.com>
2019-03-20 14:47:37 +0100

    Test Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutterteamproject/models/todos.dart';
 
+// This is our project file
 void main() {
   return runApp(MyApp());
 }
