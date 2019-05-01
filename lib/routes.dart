aaf48a0a4d802e702be013db1cc03a5ab88b5a84
Jacob Pyke <pyke.jacob1@gmail.com>
2019-05-01 12:58:38 +0200

    Removed Redux
    Decided that it was a good idea to remove the redux library from this project. It ended up being too confusing for a first timee project.

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,34 +1,17 @@
-import 'package:My2D2Do/redux/actions.dart';
-import 'package:My2D2Do/redux/middleware.dart';
-import 'package:My2D2Do/redux/model.dart';
-import 'package:My2D2Do/redux/reducers.dart';
-import 'package:My2D2Do/redux/viewmodel.dart';
 import 'package:My2D2Do/screens/myApp/myapp.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-import 'package:redux/redux.dart';
 
 class Routes {
   Routes() {
-    final Store<AppState> store = Store<AppState>(appStateReducer,
-        initialState: AppState.initialState(), middleware: [MiddleWare()]);
-    runApp(StoreProvider(
-        store: store,
-        child: StoreConnector<AppState, ViewModel>(
-          converter: (Store<AppState> store) => ViewModel.create(store),
-          onInit: ((store) => store.dispatch(FetchListFromFirebase())),
-          builder: (BuildContext context, ViewModel viewModel) {
-            return MaterialApp(
-              debugShowCheckedModeBanner: false,
-              title: 'My2D2Do',
-              theme: ThemeData(
-                brightness: Brightness.light,
-                primaryColor: Colors.red,
-                accentColor: Colors.green,
-              ),
-              home: MyApp(viewModel),
-            );
-          },
-        )));
+    runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My2D2Do',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.red,
+        accentColor: Colors.green,
+      ),
+      home: MyApp(),
+    ));
   }
 }
--- lib/screens/myApp/myapp.dart
+++ lib/screens/myApp/myapp.dart
@@ -1,47 +1,15 @@
-import 'package:My2D2Do/redux/viewmodel.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
-  final ViewModel viewModel;
-
-  MyApp(this.viewModel);
+  MyApp();
   @override
   Widget build(BuildContext context) {
-    if (viewModel.todo != null) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text("My 2D ToDo"),
-          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.settings))],
-        ),
-        body: Stack(
-          children: <Widget>[
-            ListView.builder(
-              itemCount: viewModel.todo.length,
-              itemBuilder: (BuildContext context, int i) {
-                String name = viewModel.todo[i].name;
-                return ListTile(
-                  onTap: () {},
-                  title: Text(name),
-                  trailing: Icon(Icons.arrow_forward_ios),
-                );
-              },
-            ),
-            Positioned(
-              bottom: 12,
-              left: MediaQuery.of(context).size.width/2 - 152,
-              child: RaisedButton(
-                elevation: 4,
-                padding: EdgeInsets.only(left: 140.0, right: 140.0, top: 12.0, bottom: 12.0),
-                color: Theme.of(context).accentColor,
-                onPressed: () {},
-                child: Icon(Icons.add_box, color: Theme.of(context).canvasColor,),
-              ), 
-            )
-          ],
-        ),
-      );
-    } else {
-      return Scaffold(body: Container());
-    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My 2D ToDo"),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.settings))],
+      ),
+      body: Text("New App, Without Redux")
+    );
   }
 }
