102fdb199e5829146b12e6817f56864110ad9e49
Jacob Pyke <pyke.jacob1@gmail.com>
2019-05-06 12:52:54 +0200

    This is a demo

--- lib/screens/mainHome.dart
+++ lib/screens/mainHome.dart
@@ -10,6 +10,8 @@ class MyApp extends StatelessWidget {
         actions: [IconButton(onPressed: () {}, icon: Icon(Icons.settings))],
       ),
       body: Text("New App, Without Redux")
+
+  // This is jacob hello 
     );
   }
 }
