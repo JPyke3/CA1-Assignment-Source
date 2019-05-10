74ea680933bb15dfd2f195cda8bd11a748f4b08a
Jacob Pyke <pyke.jacob1@gmail.com>
2019-05-10 08:10:49 +0200

    Some light formatting updates

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -6,23 +6,26 @@ import 'package:firebase_auth/firebase_auth.dart';
  Last thing i did was move the file into the Models folder, figured it would make more sense
 */
 
-
+/// The authentication Class, used for logging in users.
 class Auth { // Change: Renamed this class to just auth, probably looks a bit neater :) Totally wasnt necessary and what you did previously would have worked
     //To create new User
     // Wow im really impressed by how well you build the function. It looks super good and exactly how i would have done it
     // All i changed is the static keyword at the start of the function. All this does is make the function accessable from
     // Other files
+
+  /// The Single Firebase Authentication Instance
+  static FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  /// Function used to create a user and return the email as a string
   static Future<String> createUser(String email, String password) async{
-    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
     await firebaseAuth
         .createUserWithEmailAndPassword(
             email: email, password: password);
     return email;
   }
 
-// To sign in an existing user
+/// Function used to sign in a user and return the email as a string
   static Future<String> signIn(String email, String password) async{
-    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
     await firebaseAuth
         .signInWithEmailAndPassword(
             email: email, password: password);
