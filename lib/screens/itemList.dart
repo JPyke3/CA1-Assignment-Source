c508498436cb72a8ef87c19aebe7d15e796757f5
Neechie <brendan_neech@hotmail.com>
2019-05-26 14:42:15 +1000

    Access firebase data instead of dummy data

--- lib/screens/itemList.dart
+++ lib/screens/itemList.dart
@@ -2,7 +2,7 @@ import 'package:My2D2Do/classes/My2DItem.dart';
 import 'package:My2D2Do/classes/My2DList.dart';
 import 'package:flutter/material.dart';
 import 'package:My2D2Do/models/todos.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 
 class ItemList extends StatefulWidget {
@@ -10,29 +10,20 @@ class ItemList extends StatefulWidget {
   _ListsState createState() => _ListsState();
 }
 class _ListsState extends State<ItemList> {
-  List<My2DList> dummyData = [
-    My2DList(
-      name: 'Grocery Shopping', 
-      description: '', 
-      items: [
-        My2DItem(name: 'Apples', timeMade: 1, completed: false),
-        My2DItem(name: 'Oranges', timeMade: 1, completed: false),
-        My2DItem(name: 'Bananas', timeMade: 1, completed: false),
-    ]),
+  List<Todos> finalData = [];
   
-  ];
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: ListView.builder(
         itemBuilder: (context, i) {
-          return ListTile(title: Text(dummyData[i].name),
+          return ListTile(title: Text(finalData[i].name),
           onTap: (){
           },
           trailing: Icon(Icons.check_box_outline_blank),);
         },
-        itemCount: dummyData.length,
+        itemCount: finalData.length,
       )
       
     );
